d0f4556af7b017594b1e1b458564812bae6fcd27
Diego <diegocruzska@gmail.com>
2026-02-10 21:16:16 -0500

    Working on deployment 3

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -3,5 +3,9 @@ export function getBaseUrl() {
     return process.env.NEXT_PUBLIC_APP_URL;
   }
 
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
   return "http://localhost:3000";
 }
